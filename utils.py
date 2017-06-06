4ec13eec128167242bc48f77900c2ed1e41e79fa
Sinan Erdil <hsinanerdil@gmail.com>
2017-06-06 04:51:04 +0300

    Remove commented code.

--- utils.py
+++ utils.py
@@ -9,15 +9,12 @@ from order import Order
 def backtest(account: Account, market_info: MarketInfo, strategy):
     start_date = market_info.get_min_pair_start_time()
     end_date = market_info.get_min_pair_end_time()
+
     print('start:', datetime.fromtimestamp(start_date))
     print('end:', datetime.fromtimestamp(end_date))
+
     market_info.set_market_time(start_date)
     while market_info.get_market_time() < end_date - 300 * 6:
         account.execute_orders(market_info)
         strategy.act(account, market_info)
-        #account.sample_balance(market_info)
         market_info.inc_market_time()
-        #print('balance: ', account.get_estimated_balance(market_info))
-        #print('btc: ', account.get_balance(Currency.BTC))
-        #print('orders: ', len(list(account.get_orders())))
-        #print(list(account.get_orders()))
