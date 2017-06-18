d66ac454a872f20285d2c03f4a59dbdbfc3dc07d
Sinan Erdil <hsinanerdil@gmail.com>
2017-06-18 16:41:10 +0300

    Use rotating log files.

--- loghandlers.py
+++ loghandlers.py
@@ -1,12 +1,16 @@
-import logging
+import logging.handlers
+
+_1MB = 1024 * 1024 * 1024
+_5MB = 5 * _1MB
+_1GB = 1024 * _1MB
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-file_handler_debug = logging.FileHandler('log/debug.log')
+file_handler_debug = logging.handlers.RotatingFileHandler('log/debug.log', maxBytes=_1GB, backupCount=2)
 file_handler_debug.setLevel(logging.DEBUG)
 file_handler_debug.setFormatter(formatter)
 
-file_handler_info= logging.FileHandler('log/info.log')
+file_handler_info= logging.handlers.RotatingFileHandler('log/info.log', maxBytes=_5MB, backupCount=10)
 file_handler_info.setLevel(logging.INFO)
 file_handler_info.setFormatter(formatter)
 
