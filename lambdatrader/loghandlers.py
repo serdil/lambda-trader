af72bb10a5c62336d5187102e0947dff6730ce57
Sinan Erdil <hsinanerdil@gmail.com>
2017-06-23 14:26:34 +0300

    Fix log directory path.

--- lambdatrader/loghandlers.py
+++ lambdatrader/loghandlers.py
@@ -1,4 +1,5 @@
 import logging.handlers
+import os
 
 from telegram_handler import TelegramHandler
 
@@ -10,13 +11,17 @@ _1MB = 1024 * 1024 * 1024
 _5MB = 5 * _1MB
 _1GB = 1024 * _1MB
 
+LOG_FOLDER_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'log')
+DEBUG_LOG_PATH = os.path.join(LOG_FOLDER_PATH, 'debug.log')
+INFO_LOG_PATH = os.path.join(LOG_FOLDER_PATH, 'info.log')
+
 formatter = logging.Formatter(BOT_NAME + ': ' + '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-file_handler_debug = logging.handlers.RotatingFileHandler('log/debug.log', maxBytes=_1GB, backupCount=1)
+file_handler_debug = logging.handlers.RotatingFileHandler(DEBUG_LOG_PATH, maxBytes=_1GB, backupCount=1)
 file_handler_debug.setLevel(logging.DEBUG)
 file_handler_debug.setFormatter(formatter)
 
-file_handler_info= logging.handlers.RotatingFileHandler('log/info.log', maxBytes=_5MB, backupCount=10)
+file_handler_info= logging.handlers.RotatingFileHandler(INFO_LOG_PATH, maxBytes=_5MB, backupCount=10)
 file_handler_info.setLevel(logging.INFO)
 file_handler_info.setFormatter(formatter)
 
