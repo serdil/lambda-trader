96aebba234657c2994358999e2ebf390dfaac808
Sinan Erdil <hsinanerdil@gmail.com>
2017-06-25 02:12:12 +0300

    rollback

--- lambdatrader/backtestdriver.py
+++ lambdatrader/backtestdriver.py
@@ -8,7 +8,11 @@ market_info = get_past_market_info()
 
 account = Account({'BTC': 100})
 
-backtest.backtest(account, market_info, [VolumeStrategy(1), VolumeStrategy(2), VolumeStrategy(3), VolumeStrategy(4), VolumeStrategy(5), VolumeStrategy(6), VolumeStrategy(7), VolumeStrategy(8), VolumeStrategy(9), VolumeStrategy(10), VolumeStrategy(11), VolumeStrategy(12), VolumeStrategy(13)])
+backtest.backtest(account, market_info,
+                  [VolumeStrategy(1), VolumeStrategy(2),
+                   VolumeStrategy(3), VolumeStrategy(4),
+                   VolumeStrategy(5), VolumeStrategy(6)]
+                  )
 
 print(account.get_estimated_balance(market_info))
 print(list(account.get_open_orders()))
