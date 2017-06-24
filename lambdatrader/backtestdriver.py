85ac12e4ba1595f9efe3655f4dbf9f005a8b09c1
Sinan Erdil <hsinanerdil@gmail.com>
2017-06-24 03:31:54 +0300

    save

--- lambdatrader/backtestdriver.py
+++ lambdatrader/backtestdriver.py
@@ -1,6 +1,6 @@
 from backtesting.account import Account
 from backtesting.pastdata import get_past_market_info
-from strategy.strategy import Strategy
+from strategy.strategy import Strategy, VolumeStrategy
 
 from backtesting import backtest
 
@@ -8,7 +8,7 @@ market_info = get_past_market_info()
 
 account = Account({'BTC': 100})
 
-backtest.backtest(account, market_info, Strategy())
+backtest.backtest(account, market_info, VolumeStrategy())
 
 print(account.get_estimated_balance(market_info))
 print(list(account.get_open_orders()))
