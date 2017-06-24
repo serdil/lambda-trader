8301ff3ebaa0538b6e701dccd75f2dd099259e0f
Sinan Erdil <hsinanerdil@gmail.com>
2017-06-24 22:15:28 +0300

    Add new utilities.

--- lambdatrader/utils.py
+++ lambdatrader/utils.py
@@ -12,6 +12,22 @@ def pair_first(pair):
 def pair_second(pair):
     return pair[pair.index('_')+1:]
 
+
 def get_now_timestamp():
     return datetime.utcnow().timestamp()
 
+
+def timestamp_floor(timestamp):
+    return timestamp - (timestamp % 300)
+
+
+def timestamp_ceil(timestamp):
+    return timestamp - (timestamp % 300) + 300
+
+
+def get_one_day_seconds():
+    return 24 * 3600
+
+
+def get_n_day_seconds(n):
+    return get_one_day_seconds() * n
