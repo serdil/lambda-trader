2d4f13d21753248a5f4d3ee4f51abc6eee75dd28
Sinan Erdil <hsinanerdil@gmail.com>
2017-07-02 19:46:31 +0300

    Implement stats over periods except shortest_no_drawdown_window.

--- lambdatrader/evaluation/evaluation.py
+++ lambdatrader/evaluation/evaluation.py
@@ -1,5 +1,6 @@
 from _bisect import bisect_left, bisect_right
 
+import numpy
 from blist import sorteddict
 
 
@@ -14,22 +15,22 @@ class Evaluator:
     def get_trading_info(self):
         return self.__trading_info
 
-    def calc_metrics_for_period(self, start_date, end_date):
-        number_of_trades = self.__number_of_trades(start_date, end_date)
+    def calc_stats_for_period(self, start_date, end_date):
+        num_of_trades = self.__num_of_trades(start_date, end_date)
         maximum_drawdown = self.__max_drawdown(start_date, end_date)
         roi = self.__roi(start_date, end_date)
         success_rate = self.__success_rate(start_date, end_date)
         shortest_no_drawdown_window = self.__shortest_no_drawdown_window(start_date, end_date)
 
         return {
-            'number_of_trades': number_of_trades,
+            'num_of_trades': num_of_trades,
             'maximum_drawdown': maximum_drawdown,
             'roi': roi,
             'success_rate': success_rate,
             'shortest_no_drawdown_window': shortest_no_drawdown_window
         }
 
-    def __number_of_trades(self, start_date, end_date):
+    def __num_of_trades(self, start_date, end_date):
         return sum([1 for _ in self.__period_trades(start_date, end_date)])
 
     def __max_drawdown(self, start_date, end_date):
@@ -103,17 +104,17 @@ class Evaluator:
         return self.__balances[self.__balances.keys().bisect_right(date)]
 
     @classmethod
-    def calc_metrics_over_periods(cls, period_metrics):
-        shortest_no_drawdown_window = cls.__periods_shortest_no_drawdown_window(period_metrics)
-        median_roi = cls.__periods_median_roi(period_metrics)
-        first_q_roi = cls.__periods_first_first_q_roi(period_metrics)
-        minimum_roi = cls.__periods_minimum_roi(period_metrics)
-        median_num_of_trades = cls.__periods_median_num_of_trades(period_metrics)
-        first_q_num_of_trades = cls.__periods_first_q_num_of_trades(period_metrics)
-        minimum_num_of_trades = cls.__periods_minimum_num_of_trades(period_metrics)
-        median_success_rate = cls.__periods_median_success_rate(period_metrics)
-        first_q_success_rate = cls.__periods_first_q_success_rate(period_metrics)
-        minimum_success_rate = cls.__periods_minimum_success_rate(period_metrics)
+    def calc_stats_over_periods(cls, period_stats):
+        shortest_no_drawdown_window = cls.__periods_shortest_no_drawdown_window(period_stats)
+        median_roi = cls.__periods_median_roi(period_stats)
+        first_q_roi = cls.__periods_first_first_q_roi(period_stats)
+        minimum_roi = cls.__periods_minimum_roi(period_stats)
+        median_num_of_trades = cls.__periods_median_num_of_trades(period_stats)
+        first_q_num_of_trades = cls.__periods_first_q_num_of_trades(period_stats)
+        minimum_num_of_trades = cls.__periods_minimum_num_of_trades(period_stats)
+        median_success_rate = cls.__periods_median_success_rate(period_stats)
+        first_q_success_rate = cls.__periods_first_q_success_rate(period_stats)
+        minimum_success_rate = cls.__periods_minimum_success_rate(period_stats)
 
         return {
             'shortest_no_drawdown_window': shortest_no_drawdown_window,
@@ -129,41 +130,62 @@ class Evaluator:
         }
 
     @classmethod
-    def __periods_shortest_no_drawdown_window(cls, period_metrics):
+    def __periods_shortest_no_drawdown_window(cls, period_stats):
         pass
 
     @classmethod
-    def __periods_median_roi(cls, period_metrics):
-        pass
+    def __periods_median_roi(cls, period_stats):
+        roi_list = cls.__periods_roi_list(period_stats)
+        return numpy.median(roi_list)
 
     @classmethod
-    def __periods_first_first_q_roi(cls, period_metrics):
-        pass
+    def __periods_first_first_q_roi(cls, period_stats):
+        roi_list = cls.__periods_roi_list(period_stats)
+        return numpy.percentile(roi_list, 25)
 
     @classmethod
-    def __periods_minimum_roi(cls, period_metrics):
-        pass
+    def __periods_minimum_roi(cls, period_stats):
+        roi_list = cls.__periods_roi_list(period_stats)
+        return min(roi_list)
 
     @classmethod
-    def __periods_median_num_of_trades(cls, period_metrics):
-        pass
+    def __periods_median_num_of_trades(cls, period_stats):
+        num_of_trades = cls.__periods_num_of_trades_list(period_stats)
+        return numpy.median(num_of_trades)
 
     @classmethod
-    def __periods_first_q_num_of_trades(cls, period_metrics):
-        pass
+    def __periods_first_q_num_of_trades(cls, period_stats):
+        num_of_trades = cls.__periods_num_of_trades_list(period_stats)
+        return numpy.percentile(num_of_trades, 25)
 
     @classmethod
-    def __periods_minimum_num_of_trades(cls, period_metrics):
-        pass
+    def __periods_minimum_num_of_trades(cls, period_stats):
+        num_of_trades = cls.__periods_num_of_trades_list(period_stats)
+        return min(num_of_trades)
 
     @classmethod
-    def __periods_median_success_rate(cls, period_metrics):
-        pass
+    def __periods_median_success_rate(cls, period_stats):
+        success_rates = cls.__periods_success_rate_list(period_stats)
+        return numpy.median(success_rates)
 
     @classmethod
-    def __periods_first_q_success_rate(cls, period_metrics):
-        pass
+    def __periods_first_q_success_rate(cls, period_stats):
+        success_rates = cls.__periods_success_rate_list(period_stats)
+        return numpy.percentile(success_rates, 25)
 
     @classmethod
-    def __periods_minimum_success_rate(cls, period_metrics):
-        pass
+    def __periods_minimum_success_rate(cls, period_stats):
+        success_rates = cls.__periods_success_rate_list(period_stats)
+        return min(success_rates)
+    
+    @staticmethod
+    def __periods_roi_list(period_stats):
+        return [stats['roi'] for stats in period_stats]
+    
+    @staticmethod
+    def __periods_num_of_trades_list(period_stats):
+        return [stats['num_of_trades'] for stats in period_stats]
+
+    @staticmethod
+    def __periods_success_rate_list(period_stats):
+        return [stats['success_rate'] for stats in period_stats]
