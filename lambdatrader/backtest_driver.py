b718457c9ba786a8cd36e79edf352f0dcf2e9821
Sinan Erdil <hsinanerdil@gmail.com>
2017-07-02 23:17:48 +0300

    Add whole trading period statistics to backtest_driver.

--- lambdatrader/backtest_driver.py
+++ lambdatrader/backtest_driver.py
@@ -1,5 +1,6 @@
 from backtesting.account import Account
 from backtesting.marketinfo import BacktestMarketInfo
+from evaluation.evaluation import Evaluator
 from history.store import CandlestickStore
 from strategy.backtest import BacktestStrategy
 
@@ -12,10 +13,11 @@ market_info = BacktestMarketInfo(CandlestickStore.get_instance())
 account = Account({'BTC': 100})
 
 start_date = market_info.get_max_pair_end_time() - ONE_DAY * 1
+end_date = market_info.get_max_pair_end_time()
 
 strategy = BacktestStrategy()
 
-backtest.backtest(account, market_info, strategy, start=start_date)
+backtest.backtest(account, market_info, strategy, start=start_date, end=end_date)
 
 print()
 print('Backtest Complete!')
@@ -24,8 +26,16 @@ print()
 print('Estimated Balance:', account.get_estimated_balance(market_info))
 print('Open Orders:', list(account.get_open_orders()))
 
+trading_info = strategy.get_trading_info()
+
 print()
 print('Trading Info:')
 
 print()
-print(strategy.get_trading_info())
+print(trading_info)
+
+evaluator = Evaluator(trading_info)
+stats = evaluator.calc_stats_for_period(start_date, end_date)
+
+print('Statistics over whole Trading Period:')
+print(stats)
