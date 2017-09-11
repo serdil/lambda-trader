4f9217ee016764507f64c901607e2e4f45f2ef76
Sinan Erdil <hsinanerdil@gmail.com>
2017-09-12 02:29:37 +0300

    Try new rate limit.

--- lambdatrader/polx/polxclient.py
+++ lambdatrader/polx/polxclient.py
@@ -2,6 +2,6 @@ from poloniex import Poloniex, Coach
 
 from lambdatrader.config import POLONIEX_API_SECRET, POLONIEX_API_KEY
 
-coach = Coach(timeFrame=1.0, callLimit=2)
+coach = Coach(timeFrame=3.0, callLimit=2)
 
 polo = Poloniex(key=POLONIEX_API_KEY, secret=POLONIEX_API_SECRET)
