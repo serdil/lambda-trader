b3af04b7363c031e9292aeffbc928299babd9513
Sinan Erdil <hsinanerdil@gmail.com>
2017-09-12 02:32:14 +0300

    Refactor.

--- lambdatrader/polx/polxclient.py
+++ lambdatrader/polx/polxclient.py
@@ -2,6 +2,6 @@ from poloniex import Poloniex, Coach
 
 from lambdatrader.config import POLONIEX_API_SECRET, POLONIEX_API_KEY
 
-coach = Coach(timeFrame=1, callLimit=3)
+coach = Coach(timeFrame=1.0, callLimit=3)
 
 polo = Poloniex(key=POLONIEX_API_KEY, secret=POLONIEX_API_SECRET, coach=coach)
