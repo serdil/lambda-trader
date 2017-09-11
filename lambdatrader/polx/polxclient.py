5db297ad8b118ed3427eb4fb02dcf43eb5373d2e
Sinan Erdil <hsinanerdil@gmail.com>
2017-09-11 22:41:40 +0300

    Decrease requests per second to 4.
    (cherry picked from commit 58d208b)

--- lambdatrader/polx/polxclient.py
+++ lambdatrader/polx/polxclient.py
@@ -1,5 +1,7 @@
-from poloniex import Poloniex
+from poloniex import Poloniex, Coach
 
 from lambdatrader.config import POLONIEX_API_SECRET, POLONIEX_API_KEY
+
+coach = Coach(timeFrame=1.0, callLimit=4)
 
 polo = Poloniex(key=POLONIEX_API_KEY, secret=POLONIEX_API_SECRET)
