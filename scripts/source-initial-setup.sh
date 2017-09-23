569370094d04dfa660fae4d21820124112b3f12a
Sinan Erdil <hsinanerdil@gmail.com>
2017-09-23 19:09:51 +0300

    Add fabric venv to source-initial-setup.sh

--- scripts/source-initial-setup.sh
+++ scripts/source-initial-setup.sh
@@ -1,3 +1,9 @@
 source scripts/source-make-venv.sh
+source scripts/source-make-fabric-venv.sh
+
+source scripts/source-activate-fabric-venv.sh
+make install-fabric-deps
+deactivate
+
 source scripts/source-activate-venv.sh
 make install-deps
