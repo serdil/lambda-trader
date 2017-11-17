a14e3fc938773ebce361f0e44897ac9c612b2bc9
Sinan Erdil <hsinanerdil@gmail.com>
2017-11-17 11:34:19 +0300

    Add sudo to fabfile.

--- fabfile.py
+++ fabfile.py
@@ -2,14 +2,17 @@ from fabric.api import run
 from fabric.context_managers import cd
 from fabric.contrib.project import rsync_project
 from fabric.operations import put
+from fabric.state import env
+
+env.use_ssh_config = True
 
 
 def install_dtach():
-    run('apt install dtach')
+    run('sudo apt install dtach')
 
 
 def install_pip3():
-    run('apt install python3-pip')
+    run('sudo apt install python3-pip')
 
 
 def install_programs():
@@ -19,15 +22,15 @@ def install_programs():
 
 def install_docker():
     put('./scripts/remote/install-docker.sh', './install-docker.sh')
-    run('chmod +x ./install-docker.sh')
-    run('./install-docker.sh')
+    run('sudo chmod +x ./install-docker.sh')
+    run('sudo ./install-docker.sh')
     run('rm ./install-docker.sh')
 
 
 def put_ssh_key():
     put('./exclude/ssh/id_rsa', '.ssh/id_rsa')
     put('./exclude/ssh/id_rsa.pub', '.ssh/id_rsa.pub')
-    run('chmod 400 .ssh/id_rsa')
+    run('sudo chmod 400 .ssh/id_rsa')
 
 
 def runbg(cmd, sockname="dtach"):
