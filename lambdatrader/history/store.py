7319e43719e05cade3e7034798623634a3e2889e
Sinan Erdil <hsinanerdil@gmail.com>
2017-12-12 00:07:58 +0300

    Omit CVC.

--- lambdatrader/history/store.py
+++ lambdatrader/history/store.py
@@ -29,7 +29,8 @@ class CandlestickStore:
             self.__sync_with_existing_pairs_in_db()
 
         def get_pairs(self):
-            return list(filter(lambda pair: pair not in ['BTC_NOTE', 'BTC_SJCX', 'BTC_NAUT'],
+            return list(filter(lambda pair: pair not in ['BTC_NOTE', 'BTC_SJCX', 'BTC_NAUT',
+                                                         'BTC_CVC'],
                                list(set(self.__history.keys()) | set(self.__chunks_in_memory.keys())
                                     | set(self.__chunks_in_db.keys()) | self.__synced_pairs)))
 
