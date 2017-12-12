62a935c8a2d877c89e8bfab787e575099e80311c
Sinan Erdil <hsinanerdil@gmail.com>
2017-12-12 15:44:39 +0300

    Change trade signal log level to debug.

--- lambdatrader/signals/signals.py
+++ lambdatrader/signals/signals.py
@@ -182,7 +182,7 @@ class DynamicRetracementSignalGenerator(BaseSignalGenerator):  # TODO deduplicat
             trade_signal = TradeSignal(date=market_date, exchange=None, pair=pair, entry=entry,
                                        success_exit=success_exit, failure_exit=failure_exit)
 
-            self.logger.info('trade_signal:%s', str(trade_signal))
+            self.logger.debug('trade_signal:%s', str(trade_signal))
 
             return trade_signal
 
