588d460f9a3469607e6b351938e57ffcdc9a880b
Sinan Erdil <hsinanerdil@gmail.com>
2017-12-12 14:06:55 +0300

    Add info log.

--- lambdatrader/sync_polx_candlesticks.py
+++ lambdatrader/sync_polx_candlesticks.py
@@ -9,9 +9,14 @@ logger = getLogger(__name__)
 market_info = PolxMarketInfo(candlestick_store=CandlestickStore.get_instance(),
                              async_fetch_ticker=False, async_fetch_candlesticks=False)
 
+first_fetch = True
+
 while True:
     try:
         market_info.fetch_candlesticks()
+        if first_fetch:
+            logger.info('first fetch completed')
+            first_fetch = False
         sleep(1)
     except Exception:
         logger.exception('unhandled exception in sync_polx_candlesticks. retrying in 10 seconds...')
