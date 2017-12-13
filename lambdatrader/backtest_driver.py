6e5ace3e42c46bbcf6147e365a646d074386cb60
Sinan Erdil <hsinanerdil@gmail.com>
2017-12-14 00:35:28 +0300

    Use DynamicRetracementSignalGenerator in backtest_driver.
    (cherry picked from commit 968a01d)

--- lambdatrader/backtest_driver.py
+++ lambdatrader/backtest_driver.py
@@ -1,13 +1,15 @@
 from pprint import pprint
 
+from lambdatrader.backtesting import backtest
 from lambdatrader.backtesting.account import BacktestingAccount
 from lambdatrader.backtesting.marketinfo import BacktestingMarketInfo
 from lambdatrader.config import BACKTESTING_NUM_DAYS, BACKTESTING_END_OFFSET_DAYS
 from lambdatrader.evaluation.utils import statistics_over_periods, period_statistics
 from lambdatrader.executors.executors import SignalExecutor
 from lambdatrader.history.store import CandlestickStore
-from lambdatrader.backtesting import backtest
-from lambdatrader.signals.signals import RetracementSignalGenerator
+from lambdatrader.signals.signals import (
+    DynamicRetracementSignalGenerator,
+)
 
 ONE_DAY = 24 * 3600
 
@@ -22,7 +24,7 @@ start_date = market_info.get_max_pair_end_time() - 1 * BACKTEST_NUM_DAYS - BACKT
 end_date = market_info.get_max_pair_end_time() - 0 * BACKTEST_NUM_DAYS - BACKTEST_END_OFFSET_DAYS
 
 signal_generators = [
-        RetracementSignalGenerator(market_info=market_info)
+        DynamicRetracementSignalGenerator(market_info=market_info)
     ]
 signal_executor = SignalExecutor(market_info=market_info, account=account)
 
