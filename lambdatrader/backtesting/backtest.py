7abcc7509dd9bced90b4eb2de22250ec86fb3acf
Sinan Erdil <hsinanerdil@gmail.com>
2018-01-02 22:28:01 +0300

    Refactor.

--- lambdatrader/backtesting/backtest.py
+++ lambdatrader/backtesting/backtest.py
@@ -7,14 +7,15 @@ from lambdatrader.utilities.utils import date_ceil, date_floor
 
 
 def backtest(account: BacktestingAccount, market_info: BacktestingMarketInfo,
-             signal_generators, signal_executor, start=0, end=9999999999):
+             signal_generators, signal_executor, start=0, end=9999999999, silent=False):
     normalized_start = date_ceil(start)
     normalized_end = date_floor(end)
     start_date = max(market_info.get_min_pair_start_time(), normalized_start)
     end_date = min(market_info.get_max_pair_end_time(), normalized_end)
 
-    print('start:', datetime.fromtimestamp(start_date))
-    print('end:', datetime.fromtimestamp(end_date))
+    if not silent:
+        print('start:', datetime.fromtimestamp(start_date))
+        print('end:', datetime.fromtimestamp(end_date))
 
     market_info.set_market_date(start_date)
 
@@ -24,9 +25,9 @@ def backtest(account: BacktestingAccount, market_info: BacktestingMarketInfo,
     tracked_signals = []
     while market_info.get_market_date() < end_date:
         account.execute_orders()
-        signals = \
-            list(itertools.chain.from_iterable([list(generator.generate_signals(tracked_signals=tracked_signals))
-                                                for generator in signal_generators]))
+        generator_signals = [list(generator.generate_signals(tracked_signals=tracked_signals))
+                             for generator in signal_generators]
+        signals = list(itertools.chain.from_iterable(generator_signals))
         tracked_signals = signal_executor.act(signals=signals)
         market_info.inc_market_time()
 
