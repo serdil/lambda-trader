0f2a15a5c78dce2c4fcfb014872dbd4268d4e255
Sinan Erdil <hsinanerdil@gmail.com>
2018-01-28 20:08:54 +0300

    Add get_feature_funcs_iter.

--- lambdatrader/signals/data_analysis/features.py
+++ lambdatrader/signals/data_analysis/features.py
@@ -1,10 +1,32 @@
 from typing import List
 
 from lambdatrader.backtesting.marketinfo import BacktestingMarketInfo
-from lambdatrader.constants import PeriodEnum
+from lambdatrader.constants import PeriodEnum, M5, M15, H, H4, D
 from lambdatrader.indicator_functions import IndicatorEnum
 from lambdatrader.signals.data_analysis.datasets import Feature
 from lambdatrader.signals.utils import get_candle, get_indicator
+
+LOOKBACK_NUM_CANDLES = 15
+
+
+def get_feature_funcs_iter():
+    for candle_period in [M5, M15, H, H4, D]:
+        yield make_ohcl_delta(LOOKBACK_NUM_CANDLES, candle_period)
+        yield make_volume(LOOKBACK_NUM_CANDLES, candle_period)
+
+        for period in fib_seq():
+            yield make_sma_delta(LOOKBACK_NUM_CANDLES, candle_period, period)
+            yield make_rsi(LOOKBACK_NUM_CANDLES, candle_period, period)
+            yield make_atr(LOOKBACK_NUM_CANDLES, candle_period, period)
+
+
+def fib_seq():
+    yield 3
+    yield 5
+    yield 8
+    yield 13
+    yield 21
+    yield 34
 
 
 def make_ohcl_delta(num_candles, candle_period: PeriodEnum):
@@ -30,19 +52,15 @@ def make_volume(num_candles, candle_period):
             candle = get_candle(market_info, pair, i, period=candle_period)
             yield Feature(name='volume_period_{}_offset_{}'.format(candle_period.name, i),
                           value=candle.volume)
+    return feature_volume
 
 
-def make_sma_delta(num_candles, candle_period: PeriodEnum, sma_period):
-    def feature_sma_delta(market_info: BacktestingMarketInfo, pair):
-        current_sma = get_indicator(market_info, pair,
-                                    IndicatorEnum.SMA, [sma_period], 0, candle_period)[0]
-        for i in range(num_candles):
-            sma = get_indicator(market_info, pair,
-                                IndicatorEnum.SMA, [sma_period], i, candle_period)[0]
-            yield Feature(name='sma_delta_sma_period_{}_candle_period_{}_offset_{}'
-                          .format(sma_period, candle_period.name, i),
-                          value=(current_sma - sma) / current_sma)
-    return feature_sma_delta
+def make_rsi(num_candles, candle_period: PeriodEnum, rsi_period):
+    return make_indicator(num_candles, candle_period, IndicatorEnum.RSI, [rsi_period])
+
+
+def make_atr(num_candles, candle_period: PeriodEnum, atr_period):
+    return make_indicator(num_candles, candle_period, IndicatorEnum.RSI, [atr_period])
 
 
 def make_indicator(num_candles, candle_period: PeriodEnum, indicator: IndicatorEnum, args: List):
@@ -54,6 +72,11 @@ def make_indicator(num_candles, candle_period: PeriodEnum, indicator: IndicatorE
                               .format(indicator.name, join_list(args),
                                       val_ind, candle_period.name, i),
                               value=value)
+    return feature_indicator
+
+
+def make_sma_delta(num_candles, candle_period, sma_period):
+    return make_indicator_delta(num_candles, candle_period, IndicatorEnum.SMA, [sma_period])
 
 
 def make_indicator_delta(num_candles, candle_period: PeriodEnum,
@@ -67,6 +90,7 @@ def make_indicator_delta(num_candles, candle_period: PeriodEnum,
                               .format(indicator.name, join_list(args),
                                       val_ind, candle_period.name, i),
                               value=(current_vals[val_ind] - value) / current_vals[val_ind])
+    return feature_indicator_delta
 
 
 def join_list(lst):
