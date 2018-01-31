dc7eaa296391a991363c946072f50c13339fbcad
Sinan Erdil <hsinanerdil@gmail.com>
2018-02-01 00:28:13 +0300

    Add random forest example.

--- lambdatrader/signals/data_analysis/learning/dummy/scikit_learn_dummy.py
+++ lambdatrader/signals/data_analysis/learning/dummy/scikit_learn_dummy.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn import datasets, svm
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.linear_model import LogisticRegression, LinearRegression, Lasso
 from sklearn.neighbors import KNeighborsClassifier
 
 iris = datasets.load_iris()
@@ -28,11 +30,13 @@ print(prediction)
 # Reshaping
 
 print(digits.images.shape)
-ax = plt.imshow(digits.images[-1], cmap=plt.cm.gray_r)
+# ax = plt.imshow(digits.images[-1], cmap=plt.cm.gray_r)
 # plt.show()
 
 data = digits.images.reshape((digits.images.shape[0], -1))
 
+
+print('KNN')
 
 X_digits = digits.data
 y_digits = digits.target
@@ -55,6 +59,8 @@ print(predictions)
 
 print('knn score:', knn.score(X_digits_test, y_digits_test))
 
+print('LOGISTIC REGRESSION')
+
 logistic = LogisticRegression(C=1e5)
 
 logistic.fit(X_digits_train, y_digits_train)
@@ -66,3 +72,89 @@ print(predictions)
 
 print('logistic score:', logistic.score(X_digits_test, y_digits_test))
 
+
+diabetes = datasets.load_diabetes()
+diabetes_X_train = diabetes.data[:-20]
+diabetes_X_test  = diabetes.data[-20:]
+diabetes_y_train = diabetes.target[:-20]
+diabetes_y_test  = diabetes.target[-20:]
+
+print('LINEAR REGRESSION')
+
+regr = LinearRegression()
+regr.fit(diabetes_X_train, diabetes_y_train)
+
+print('regr.coef_:', regr.coef_)
+
+# The mean square error
+mse = np.mean((regr.predict(diabetes_X_test)-diabetes_y_test)**2)
+
+# Explained variance score: 1 is perfect prediction
+# and 0 means that there is no linear relationship
+# between X and y.
+score = regr.score(diabetes_X_test, diabetes_y_test)
+
+print('mse:', mse, 'score:', score)
+
+print('LASSO')
+
+alphas = np.logspace(-4, -1, 6)
+
+regr = Lasso()
+scores = [regr.set_params(alpha=alpha
+            ).fit(diabetes_X_train, diabetes_y_train
+            ).score(diabetes_X_test, diabetes_y_test)
+       for alpha in alphas]
+best_alpha = alphas[scores.index(max(scores))]
+regr.alpha = best_alpha
+regr.fit(diabetes_X_train, diabetes_y_train)
+
+mse = np.mean((regr.predict(diabetes_X_test)-diabetes_y_test)**2)
+score = regr.score(diabetes_X_test, diabetes_y_test)
+
+print('regr.coef_', regr.coef_)
+print('mse:', mse, 'score:', score)
+
+
+print('RANDOM FOREST')
+
+X_digits = digits.data
+y_digits = digits.target
+
+n_samples = len(digits.target)
+
+X_digits_train = X_digits[:int(0.9 * n_samples)]
+y_digits_train = y_digits[:int(0.9 * n_samples)]
+
+X_digits_test = X_digits[int(0.9 * n_samples):]
+y_digits_test = y_digits[int(0.9 * n_samples):]
+
+rf = RandomForestClassifier(n_estimators=10)
+rf.fit(X_digits_train, y_digits_train)
+
+predictions = rf.predict(X_digits_test)
+
+print(y_digits_test)
+print(predictions)
+
+print('rf score:', rf.score(X_digits_test, y_digits_test))
+
+
+print('RANDOM FOREST REGRESSION')
+
+regr = RandomForestRegressor(n_estimators=100)
+regr.fit(diabetes_X_train, diabetes_y_train)
+
+# The mean square error
+mse = np.mean((regr.predict(diabetes_X_test)-diabetes_y_test)**2)
+
+# Explained variance score: 1 is perfect prediction
+# and 0 means that there is no linear relationship
+# between X and y.
+score = regr.score(diabetes_X_test, diabetes_y_test)
+
+print('mse:', mse, 'score:', score)
+
+print('shapes:')
+print(diabetes_X_train.shape)
+print(diabetes_y_train.shape)
--- lambdatrader/signals/data_analysis/learning/dummy/scikit_learn_price_data_dummy.py
+++ lambdatrader/signals/data_analysis/learning/dummy/scikit_learn_price_data_dummy.py
@@ -1,3 +1,6 @@
+from sklearn import metrics
+from sklearn.ensemble import RandomForestRegressor
+
 from lambdatrader.backtesting.marketinfo import BacktestingMarketInfo
 from lambdatrader.candlestickstore import CandlestickStore
 from lambdatrader.exchanges.enums import ExchangeEnum
@@ -14,7 +17,7 @@ print(market_info)
 latest_market_date = market_info.get_max_pair_end_time()
 
 dataset_symbol = 'BTC_ETH'
-dataset_start_date = latest_market_date - seconds(days=7, minutes=30)
+dataset_start_date = latest_market_date - seconds(days=7, hours=24)
 dataset_end_date = latest_market_date - seconds(days=7)
 
 dataset = create_pair_dataset_from_history(market_info=market_info,
@@ -37,3 +40,27 @@ print(y)
 
 print('X shape:', X.shape)
 print('y shape:', y.shape)
+
+n_samples = len(y)
+
+train_ratio = 0.7
+
+X_train = X[:int(train_ratio * n_samples)]
+y_train = y[:int(train_ratio * n_samples)]
+
+X_test = X[int(train_ratio * n_samples):]
+y_test = y[int(train_ratio * n_samples):]
+
+
+rf = RandomForestRegressor(n_estimators=10)
+rf.fit(X_train, y_train)
+
+predictions = rf.predict(X_test)
+
+mse = metrics.mean_squared_error(y_test, predictions)
+score = rf.score(X_test, y_test)
+
+print('predictions:', predictions * 100)
+print('real:', y_test * 100)
+
+print('mse:', mse, 'score:', score)
