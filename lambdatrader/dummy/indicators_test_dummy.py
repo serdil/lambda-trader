6a7f381d0672b2257fe17a8a7159e899f9101286
Sinan Erdil <hsinanerdil@gmail.com>
2018-01-31 20:21:46 +0300

    Add indicators_test_dummy and general_dummy.

---
+++ lambdatrader/dummy/indicators_test_dummy.py
@@ -0,0 +1,21 @@
+from lambdatrader.backtesting.marketinfo import BacktestingMarketInfo
+from lambdatrader.candlestickstore import CandlestickStore
+from lambdatrader.constants import H4, D
+from lambdatrader.exchanges.enums import POLONIEX
+from lambdatrader.indicator_functions import IndicatorEnum
+from lambdatrader.utilities.utils import seconds
+
+market_info = BacktestingMarketInfo(candlestick_store=CandlestickStore.get_for_exchange(POLONIEX))
+
+market_date = market_info.get_max_pair_end_time() - seconds(days=7)
+
+market_info.set_market_date(market_date)
+
+macd_vals = market_info.get_indicator('BTC_ETH', IndicatorEnum.MACD, [12, 26, 9], period=H4)
+
+print(macd_vals)
+
+for i in range(365, -1, -1):
+    macd_vals = market_info.get_indicator('BTC_ETH', IndicatorEnum.MACD, [12, 26, 9],
+                                          ind=i, period=D)
+    print(macd_vals)
