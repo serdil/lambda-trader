d7b3fbc5a0f00f71bcc0ee7a9cc4764dd00daa72
Sinan Erdil <hsinanerdil@gmail.com>
2018-02-01 02:49:31 +0300

    Crucial bugfix in make_cont_max_price_in_future.

--- lambdatrader/signals/data_analysis/values.py
+++ lambdatrader/signals/data_analysis/values.py
@@ -11,7 +11,7 @@ CLASS_INCREASE_FLAT_DOWN = 'flat_down'
 
 
 def make_cont_close_price_in_fifteen_mins_cont():
-    return make_close_price_in_future(num_candles=3, candle_period=M5)
+    return make_cont_close_price_in_future(num_candles=3, candle_period=M5)
 
 
 def make_class_max_price_in_fifteen_mins():
@@ -28,10 +28,10 @@ def make_class_max_price_in_fifteen_mins():
 
 
 def make_cont_max_price_in_fifteen_mins():
-    return make_max_price_in_future(num_candles=3, candle_period=M5)
+    return make_cont_max_price_in_future(num_candles=3, candle_period=M5)
 
 
-def make_close_price_in_future(num_candles, candle_period: PeriodEnum):
+def make_cont_close_price_in_future(num_candles, candle_period: PeriodEnum):
     def value_cont_close_price_in_future(market_info: BacktestingMarketInfo, pair):
         last_candle = market_info.get_pair_period_candlestick(pair, 0, period=candle_period)
         candle_in_future = market_info.get_pair_period_candlestick(pair=pair,
@@ -42,13 +42,13 @@ def make_close_price_in_future(num_candles, candle_period: PeriodEnum):
     return value_cont_close_price_in_future
 
 
-def make_max_price_in_future(num_candles, candle_period: PeriodEnum):
+def make_cont_max_price_in_future(num_candles, candle_period: PeriodEnum):
     def value_cont_max_price_in_future(market_info: BacktestingMarketInfo, pair):
         last_candle = market_info.get_pair_period_candlestick(pair, 0, period=candle_period)
         highest_until_future = float('-inf')
         for i in range(-1, -num_candles-1, -1):
             candle = market_info.get_pair_period_candlestick(pair=pair,
-                                                             ind=-i,
+                                                             ind=i,
                                                              period=candle_period)
             highest_until_future = max(highest_until_future, candle.high)
         return highest_until_future / last_candle.close - 1.0
