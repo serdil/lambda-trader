ebd076ee24890baa5af1a41f26c598bec0ae1132
Sinan Erdil <hsinanerdil@gmail.com>
2018-02-01 16:08:35 +0300

    Add learning_utils. Add new examples.

---
+++ lambdatrader/signals/data_analysis/learning/dummy/learning_utils_dummy.py
@@ -0,0 +1,96 @@
+import pprint
+import time
+from operator import itemgetter
+
+import numpy as np
+from sklearn import metrics
+
+
+def train_and_test_model(dataset, model, classification_task=False, train_ratio=0.7):
+    _feature_names = dataset.get_first_feature_names()
+
+    X = dataset.get_numpy_feature_matrix()
+    y = dataset.get_numpy_value_array()
+
+    _n_samples = len(y)
+    _split_ind = int(train_ratio * _n_samples)
+
+    X_train = X[:_split_ind]
+    y_train = y[:_split_ind]
+
+    X_test = X[_split_ind:]
+    y_test = y[_split_ind:]
+
+    _start_time = time.time()
+    model.fit(X_train, y_train)
+    training_time = time.time() - _start_time
+
+
+    _importance = model.feature_importances_
+    _name_importance = zip(_feature_names, _importance)
+    name_importance_sorted = list(reversed(sorted(_name_importance, key=itemgetter(1))))[:20]
+
+    stats = {
+        'training_time': training_time,
+        'importance': name_importance_sorted,
+    }
+
+    test_stats = test_model(model, X_test, y_test, classification_task=classification_task)
+
+    stats.update(test_stats)
+    return stats
+
+
+def test_model(model, x_test, y_test, classification_task=False):
+    pred = model.predict(x_test)
+
+    if not classification_task:
+        mse = metrics.mean_squared_error(y_test, pred)
+        score = model.score(x_test, y_test)
+
+        pred_sign = pred > 0
+        real_sign = y_test > 0
+
+        sign_equal = np.equal(real_sign, pred_sign)
+
+        real_positive_ratio = np.sum(real_sign) / float(len(real_sign))
+        sign_equal_ratio = np.sum(sign_equal) / float(len(sign_equal))
+
+
+        return {
+            'pred': pred,
+            'real': y_test,
+            'pred_sign': pred_sign,
+            'real_sign': real_sign,
+            'sign_equal_ratio': sign_equal_ratio,
+            'real_positive_ratio': real_positive_ratio,
+            'mse': mse,
+            'score': score
+        }
+
+
+def print_model_stats(stats):
+    print('IMPORTANCES:')
+    for name, importance in stats['importance']:
+        print('{:<80} {}'.format(name, importance))
+    print()
+
+    real = stats['real'] * 100
+    preds = stats['pred'] * 100
+
+    real_preds = list(zip(real, preds))
+    print('real, pred:', pprint.pformat(real_preds))
+
+    real_sign = stats['real_sign']
+    pred_sign = stats['pred_sign']
+
+    real_pred_sign = list(zip(real_sign, pred_sign))
+    print('real_sign, pred_sign:', pprint.pformat(real_pred_sign))
+
+    print()
+    print('training time:', stats['training_time'])
+    print('mse:', stats['mse'], 'score:', stats['score'])
+
+    print()
+    print('real positive ratio:', stats['real_positive_ratio'])
+    print('sign equal ratio:', stats['sign_equal_ratio'])
--- lambdatrader/signals/data_analysis/learning/dummy/rfr_close_delta_dummy.py
+++ lambdatrader/signals/data_analysis/learning/dummy/rfr_close_delta_dummy.py
@@ -1,9 +1,3 @@
-import time
-from collections import OrderedDict
-from operator import itemgetter
-
-import numpy as np
-from sklearn import metrics
 from sklearn.ensemble import RandomForestRegressor
 
 from lambdatrader.backtesting.marketinfo import BacktestingMarketInfo
@@ -12,7 +6,12 @@ from lambdatrader.exchanges.enums import ExchangeEnum
 from lambdatrader.shelve_cache import shelve_cache_save
 from lambdatrader.signals.data_analysis.datasets import create_pair_dataset_from_history
 from lambdatrader.signals.data_analysis.features import get_feature_funcs_iter
-from lambdatrader.signals.data_analysis.values import make_cont_max_price_in_fifteen_mins
+from lambdatrader.signals.data_analysis.learning.dummy.learning_utils_dummy import (
+    train_and_test_model, print_model_stats,
+)
+from lambdatrader.signals.data_analysis.values import (
+    make_cont_close_price_in_fifteen_mins,
+)
 from lambdatrader.utilities.utils import seconds
 
 
@@ -26,7 +25,6 @@ def save(model_name, model):
 market_info = BacktestingMarketInfo(candlestick_store=
                                     CandlestickStore.get_for_exchange(ExchangeEnum.POLONIEX))
 
-print(market_info)
 
 latest_market_date = market_info.get_max_pair_end_time()
 
@@ -47,73 +45,20 @@ dataset = create_pair_dataset_from_history(market_info=market_info,
                                            start_date=dataset_start_date,
                                            end_date=dataset_end_date,
                                            feature_functions=list(get_feature_funcs_iter()),
-                                           value_function=make_cont_max_price_in_fifteen_mins(),
-                                           cache_and_get_cached=True)
+                                           value_function=make_cont_close_price_in_fifteen_mins(),
+                                           cache_and_get_cached=True,
+                                           value_function_key='close_delta_2')
 
-print('created/loaded dataset')
-
-feature_names = dataset.get_first_feature_names()
-
-X = dataset.get_numpy_feature_matrix()
-y = dataset.get_numpy_value_array()
-
-# print(feature_names)
-print(X)
-print(y)
-
-print('X shape:', X.shape)
-print('y shape:', y.shape)
-
-n_samples = len(y)
-
-train_ratio = 0.7
-split_ind = int(train_ratio * n_samples)
-
-X_train = X[:split_ind]
-y_train = y[:split_ind]
-
-X_test = X[split_ind:]
-y_test = y[split_ind:]
+print('created/loaded dataset\n')
 
 n_estimators = 100
 
 rfr = RandomForestRegressor(n_estimators=n_estimators)
 
-start_time = time.time()
-rfr.fit(X_train, y_train)
-print('training time:', time.time() - start_time)
+stats = train_and_test_model(dataset, rfr)
+print_model_stats(stats)
 
-rfr_pred = rfr.predict(X_test)
-
-rfr_mse = metrics.mean_squared_error(y_test, rfr_pred)
-rfr_score = rfr.score(X_test, y_test)
-
-pred_sign = rfr_pred > 0
-real_sign = y_test > 0
-
-sign_equal = np.equal(real_sign, pred_sign)
-
-print('predictions:', rfr_pred * 100)
-print('real:', y_test * 100)
-
-print('pred_sign:', pred_sign)
-print('real_sign:', real_sign)
-
-rfr_importance = rfr.feature_importances_
-name_importance = zip(feature_names, rfr_importance)
-name_importance_sorted = list(reversed(sorted(name_importance, key=itemgetter(1))))
-importance_dict = OrderedDict(name_importance_sorted)
-
-for name, importance in name_importance_sorted:
-    if importance != 0:
-        print(name, importance)
-
-print('mse:', rfr_mse, 'score:', rfr_score)
-
-print('real positive ratio:', np.sum(real_sign) / float(len(real_sign)))
-print('sign equal ratio:', np.sum(sign_equal) / float(len(sign_equal)))
-
-model_name = 'rfr_{}_{}'.format(dataset_len, n_estimators)
+model_name = 'rfr_close_delta_{}_{}'.format(dataset_len, n_estimators)
 
 save(model_name, rfr)
 print('saved model:', model_name)
