6f3250b73709eacade6d6ba5f3fe2e12d34ce94c
Sinan Erdil <hsinanerdil@gmail.com>
2018-02-02 19:46:16 +0300

    Parameters.

--- lambdatrader/signals/data_analysis/learning/dummy/xgboost_log_reg_dummy.py
+++ lambdatrader/signals/data_analysis/learning/dummy/xgboost_log_reg_dummy.py
@@ -43,7 +43,7 @@ market_info = BacktestingMarketInfo(candlestick_store=
 
 latest_market_date = market_info.get_max_pair_end_time()
 
-dataset_symbol = 'BTC_LTC'
+dataset_symbol = 'BTC_VTC'
 
 day_offset = 7
 
@@ -78,42 +78,63 @@ feature_names = dataset.get_first_feature_names()
 X = dataset.get_numpy_feature_matrix()
 y = dataset.get_numpy_value_array()
 
-train_ratio = 0.9
+train_ratio = 0.8
+val_ratio = 0.9
 
 n_samples = len(y)
-split_ind = int(train_ratio * n_samples)
+val_split = int(train_ratio * n_samples)
+test_split = int(val_ratio * n_samples)
 
-X_train = X[:split_ind]
-y_train = y[:split_ind]
+X_train = X[:val_split]
+y_train = y[:val_split]
 
-X_test = X[split_ind:]
-y_test = y[split_ind:]
+X_val = X[val_split:]
+y_val = y[val_split:]
+
+X_test = X[test_split:]
+y_test = y[test_split:]
 
 dtrain = xgb.DMatrix(X_train, label=y_train, feature_names=feature_names)
+dval = xgb.DMatrix(X_val, label=y_val, feature_names=feature_names)
 dtest = xgb.DMatrix(X_test, label=y_test, feature_names=feature_names)
 
-params = {'max_depth': 2, 'eta': 0.05, 'silent': 1, 'objective': 'binary:logistic'}
-evals = [(dtest, 'test_set')]
-num_round = 100000
-early_stopping_rounds = 200
+
+params = {'max_depth': 2, 'eta': 0.03, 'silent': 1, 'objective': 'binary:logistic'}
+evals = [(dval, 'val_set')]
+num_round = 1000
+early_stopping_rounds = 100
 
 bst = xgb.train(params=params,
                 dtrain=dtrain,
                 num_boost_round=num_round,
                 evals=evals,
                 early_stopping_rounds=early_stopping_rounds)
-print(bst)
+
+pred = bst.predict(dval)
+
+pred_real = list(zip(pred, y_val))
+
+sorted_by_pred = list(reversed(sorted(pred_real, key=lambda x: (x[0],x[1]))))
+sorted_by_real = list(reversed(sorted(pred_real, key=lambda x: (x[1],x[0]))))
+
+print('====VALIDATION=======VALIDATION=======VALIDATION=======VALIDATION=======VALIDATION===')
+
+print()
+print('pred, real:')
+for item1, item2 in list(zip(sorted_by_pred, sorted_by_real))[:200]:
+    print('{:30}{:30}'.format('{:.6f}, {:.6f}'.format(*item1), '{:.6f}, {:.6f}'.format(*item2)))
 
 pred = bst.predict(dtest)
-mse = sklearn_metrics.mean_squared_error(y_test, pred)
 
 pred_real = list(zip(pred, y_test))
 
 sorted_by_pred = list(reversed(sorted(pred_real, key=lambda x: (x[0],x[1]))))
 sorted_by_real = list(reversed(sorted(pred_real, key=lambda x: (x[1],x[0]))))
 
+print()
+print('+++TEST+++++++TEST+++++++TEST+++++++TEST+++++++TEST+++++++TEST+++++++TEST+++++++TEST++++')
 
 print()
 print('pred, real:')
-for item1, item2 in list(zip(sorted_by_pred, sorted_by_real))[:10000]:
+for item1, item2 in list(zip(sorted_by_pred, sorted_by_real))[:200]:
     print('{:30}{:30}'.format('{:.6f}, {:.6f}'.format(*item1), '{:.6f}, {:.6f}'.format(*item2)))
