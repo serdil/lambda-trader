4fe9a5634cfe16b1032b0d7c6091900caf4eea2d
Sinan Erdil <hsinanerdil@gmail.com>
2018-02-24 02:26:23 +0300

    Add h2o_automl_dummy.

--- lambdatrader/signals/data_analysis/learning/dummy/dummy_utils_dummy.py
+++ lambdatrader/signals/data_analysis/learning/dummy/dummy_utils_dummy.py
@@ -12,10 +12,25 @@ from lambdatrader.utilities.utils import seconds
 fsf = DFFeatureSetFactory
 
 
-def get_x_and_y_close_max_min(symbol='BTC_ETH', day_offset=120, days=500, feature_set=fsf.get_small()):
+def get_x_and_y_close_max_min(symbol='BTC_ETH', day_offset=120, days=500, feature_set=fsf.get_small(), num_candles=48):
 
-    market_info = BacktestingMarketInfo(candlestick_store=
-                                        ChunkCachingCandlestickStore.get_for_exchange(ExchangeEnum.POLONIEX))
+    ds_feature_df, ds_value_df = get_feature_df_value_df(symbol, day_offset, days, feature_set, num_candles)
+
+    max_return_v = MaxReturn(num_candles)
+    min_return_v = MinReturn(num_candles)
+    close_return_v = CloseReturn(num_candles)
+
+    X = ds_feature_df.values
+    y_close = ds_value_df[close_return_v.name].values
+    y_max = ds_value_df[max_return_v.name]
+    y_min = ds_value_df[min_return_v.name]
+
+    return X, y_close, y_max, y_min
+
+
+def get_feature_df_value_df(symbol='BTC_ETH', day_offset=120, days=500, feature_set=fsf.get_small(), num_candles=48):
+    market_info = BacktestingMarketInfo(
+        candlestick_store=ChunkCachingCandlestickStore.get_for_exchange(ExchangeEnum.POLONIEX))
 
     latest_market_date = market_info.get_max_pair_end_time()
 
@@ -27,25 +42,10 @@ def get_x_and_y_close_max_min(symbol='BTC_ETH', day_offset=120, days=500, featur
                                                datetime.utcfromtimestamp(dataset_end_date)))
     print()
 
-    num_candles = 48
+    value_set = DFFeatureSet(
+        features=[MaxReturn(num_candles), MinReturn(num_candles), CloseReturn(num_candles)])
 
-    max_return_v = MaxReturn(num_candles)
-    min_return_v = MinReturn(num_candles)
-    close_return_v = CloseReturn(num_candles)
+    ds = DFDataset.compute(pair=symbol, feature_set=feature_set, value_set=value_set,
+                           start_date=dataset_start_date, end_date=dataset_end_date)
 
-    value_set = DFFeatureSet(features=[MaxReturn(num_candles),
-                                       MinReturn(num_candles),
-                                       CloseReturn(num_candles)])
-
-    ds = DFDataset.compute(pair=symbol,
-                           feature_set=feature_set,
-                           value_set=value_set,
-                           start_date=dataset_start_date,
-                           end_date=dataset_end_date)
-
-    X = ds.feature_values
-    y_close = ds.get_value_values(close_return_v.name)
-    y_max = ds.get_value_values(max_return_v.name)
-    y_min = ds.get_value_values(min_return_v.name)
-
-    return X, y_close, y_max, y_min
+    return ds.feature_df, ds.value_df
--- lambdatrader/signals/data_analysis/learning/dummy/h2o/h2o_automl_dummy.py
+++ lambdatrader/signals/data_analysis/learning/dummy/h2o/h2o_automl_dummy.py
@@ -1,5 +1,67 @@
-from lambdatrader.signals.data_analysis.learning.dummy.dummy_utils_dummy import \
-    get_x_and_y_close_max_min
+from h2o import H2OFrame, h2o
+from h2o.automl import H2OAutoML
 
-X, y_close, y_max, y_min = get_x_and_y_close_max_min()
+from lambdatrader.signals.data_analysis.df_values import MaxReturn, MinReturn, CloseReturn
+from lambdatrader.signals.data_analysis.factories import DFFeatureSetFactory
+from lambdatrader.signals.data_analysis.learning.dummy.dummy_utils_dummy import (
+    get_x_and_y_close_max_min, get_feature_df_value_df,
+)
+from lambdatrader.utilities.utils import seconds
 
+h2o.init()
+
+num_candles = 48
+
+close_return_name = CloseReturn(num_candles).name
+max_return_name = MaxReturn(num_candles).name
+min_return_name = MinReturn(num_candles).name
+
+feature_set = DFFeatureSetFactory.get_all_periods_last_five_ohlcv()
+num_days = 500
+
+feature_df, value_df = get_feature_df_value_df(num_candles=num_candles,
+                                               days=num_days,
+                                               feature_set=feature_set)
+
+close_df = feature_df.join(value_df[close_return_name])
+
+# print(close_df)
+
+n_samples = len(close_df)
+
+validation_ratio = 0.70
+leaderboard_ratio = 0.85
+
+validation_split_ind = int(n_samples * validation_ratio)
+leaderboard_split_ind = int(n_samples * leaderboard_ratio)
+gap = num_candles
+
+close_train_df = close_df.iloc[0:validation_split_ind-gap]
+close_val_df = close_df.iloc[validation_split_ind:leaderboard_split_ind-gap]
+close_lead_df = close_df.iloc[leaderboard_split_ind:]
+
+train_frame = H2OFrame(python_obj=close_train_df)
+val_frame = H2OFrame(python_obj=close_val_df)
+lead_frame = H2OFrame(python_obj=close_lead_df)
+
+x = train_frame.columns
+y = close_return_name
+x.remove(y)
+
+# run_seconds = 10
+run_seconds = seconds(hours=8)
+
+aml = H2OAutoML(max_runtime_secs=run_seconds, nfolds=0)
+aml.train(x=x, y=y,
+          training_frame=train_frame,
+          validation_frame=val_frame,
+          leaderboard_frame=lead_frame)
+
+# View the AutoML Leaderboard
+lb = aml.leaderboard
+
+print(lb)
+
+model_path = h2o.save_model(model=aml.leader, path="/tmp/nightmodel_h2o", force=True)
+
+print('model saved:', model_path)
