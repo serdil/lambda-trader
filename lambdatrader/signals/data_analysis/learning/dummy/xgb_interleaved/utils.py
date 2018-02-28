96d3ece653482a4273527b251b2dd361190fde56
Sinan Erdil <hsinanerdil@gmail.com>
2018-02-28 21:20:26 +0300

    Update utils. Add training function that load data from dmatrix buffers.

--- lambdatrader/signals/data_analysis/learning/dummy/xgb_interleaved/utils.py
+++ lambdatrader/signals/data_analysis/learning/dummy/xgb_interleaved/utils.py
@@ -13,24 +13,26 @@ from lambdatrader.exchanges.enums import ExchangeEnum
 from lambdatrader.signals.data_analysis.df_datasets import DFDataset
 from lambdatrader.signals.data_analysis.df_features import DFFeatureSet
 from lambdatrader.signals.data_analysis.df_values import CloseReturn, MaxReturn, MinReturn
+from lambdatrader.signals.data_analysis.learning.dummy.xgb_interleaved.dmatrix_save_load_util \
+    import (
+    load_close_dmatrix, load_max_dmatrix,
+)
 from lambdatrader.utilities.utils import seconds
 
 CMMValueSet = namedtuple('CMMValueSet', ['value_set', 'close_name', 'max_name', 'min_name'])
 
 
 def get_cmm_value_set(num_candles):
-    close_return= CloseReturn(n_candles=num_candles, period=M5)
+    close_return = CloseReturn(n_candles=num_candles, period=M5)
     max_return = MaxReturn(n_candles=num_candles, period=M5)
     min_return = MinReturn(n_candles=num_candles, period=M5)
     vs = DFFeatureSet(features=[close_return, max_return, min_return])
-    return CMMValueSet(value_set=vs,
-                       close_name=close_return.name,
-                       max_name=max_return.name,
+    return CMMValueSet(value_set=vs, close_name=close_return.name, max_name=max_return.name,
                        min_name=min_return.name)
 
 
-def get_train_val_test_Xs_ys_feature_names(symbols, feature_set,
-                                           val_ratio, test_ratio, num_candles, day_offset, days):
+def get_train_val_test_Xs_ys_feature_names(symbols, feature_set, val_ratio, test_ratio, num_candles,
+                                           day_offset, days):
     market_info = BacktestingMarketInfo(
         candlestick_store=ChunkCachingCandlestickStore.get_for_exchange(ExchangeEnum.POLONIEX))
     latest_market_date = market_info.get_max_pair_end_time()
@@ -43,18 +45,13 @@ def get_train_val_test_Xs_ys_feature_names(symbols, feature_set,
                                                datetime.utcfromtimestamp(dataset_end_date)))
     print()
     cmm_vs = get_cmm_value_set(num_candles=num_candles)
-    X, y_close, y_max, f_names= (DFDataset
-                                 .compute_interleaved(pairs=symbols,
-                                                      feature_set=feature_set,
-                                                      value_set=cmm_vs.value_set,
-                                                      start_date=dataset_start_date,
-                                                      end_date=dataset_end_date,
-                                                      error_on_missing=False)
-                                 .add_feature_values()
-                                 .add_value_values(value_name=cmm_vs.close_name)
-                                 .add_value_values(value_name=cmm_vs.max_name)
-                                 .add_feature_names()
-                                 .get())
+    X, y_close, y_max, f_names = (
+    DFDataset.compute_interleaved(pairs=symbols, feature_set=feature_set,
+                                  value_set=cmm_vs.value_set, start_date=dataset_start_date,
+                                  end_date=dataset_end_date,
+                                  error_on_missing=False).add_feature_values().add_value_values(
+        value_name=cmm_vs.close_name).add_value_values(
+        value_name=cmm_vs.max_name).add_feature_names().get())
 
     n_samples = len(X)
     gap = num_candles * len(symbols)
@@ -74,35 +71,24 @@ def get_train_val_test_Xs_ys_feature_names(symbols, feature_set,
     y_close_test = y_close[test_split_ind:]
     y_max_test = y_max[test_split_ind:]
 
-    return X_train, y_close_train, y_max_train, \
-           X_val, y_close_val, y_max_val, \
-           X_test, y_close_test, y_max_test, f_names
+    return X_train, y_close_train, y_max_train, X_val, y_close_val, y_max_val, X_test, \
+           y_close_test, y_max_test, f_names
 
 
 def get_test_X_ys(symbols, feature_set, val_ratio, test_ratio, num_candles, day_offset, days,
                   **kwargs):
-    X_train, y_close_train, y_max_train,\
-    X_val, y_close_val, y_max_val, \
-    X_test, y_close_test, y_max_test, f_names = get_train_val_test_Xs_ys_feature_names(
-                                                                       symbols=symbols,
-                                                                       val_ratio=val_ratio,
-                                                                       test_ratio=test_ratio,
-                                                                       num_candles=num_candles,
-                                                                       day_offset=day_offset,
-                                                                       days=days,
-                                                                       feature_set=feature_set)
+    X_train, y_close_train, y_max_train, X_val, y_close_val, y_max_val, X_test, y_close_test, \
+    y_max_test, f_names = get_train_val_test_Xs_ys_feature_names(
+        symbols=symbols, val_ratio=val_ratio, test_ratio=test_ratio, num_candles=num_candles,
+        day_offset=day_offset, days=days, feature_set=feature_set)
     return X_test, y_close_test, y_max_test
 
 
 def get_close_dmatrix(symbols, feature_set, val_ratio, test_ratio, num_candles, day_offset, days):
     X_train, y_close_train, y_max_train, X_val, y_close_val, y_max_val, X_test, y_close_test, \
-    y_max_test, feature_names = get_train_val_test_Xs_ys_feature_names(symbols=symbols,
-                                                                       val_ratio=val_ratio,
-                                                                       test_ratio=test_ratio,
-                                                                       num_candles=num_candles,
-                                                                       day_offset=day_offset,
-                                                                       days=days,
-                                                                       feature_set=feature_set)
+    y_max_test, feature_names = get_train_val_test_Xs_ys_feature_names(
+        symbols=symbols, val_ratio=val_ratio, test_ratio=test_ratio, num_candles=num_candles,
+        day_offset=day_offset, days=days, feature_set=feature_set)
 
     dtrain_close = xgb.DMatrix(X_train, label=y_close_train, feature_names=feature_names)
     dval_close = xgb.DMatrix(X_val, label=y_close_val, feature_names=feature_names)
@@ -113,13 +99,9 @@ def get_close_dmatrix(symbols, feature_set, val_ratio, test_ratio, num_candles, 
 
 def get_max_dmatrix(symbols, feature_set, val_ratio, test_ratio, num_candles, day_offset, days):
     X_train, y_close_train, y_max_train, X_val, y_close_val, y_max_val, X_test, y_close_test, \
-    y_max_test, feature_names = get_train_val_test_Xs_ys_feature_names(symbols=symbols,
-                                                                       val_ratio=val_ratio,
-                                                                       test_ratio=test_ratio,
-                                                                       num_candles=num_candles,
-                                                                       day_offset=day_offset,
-                                                                       days=days,
-                                                                       feature_set=feature_set)
+    y_max_test, feature_names = get_train_val_test_Xs_ys_feature_names(
+        symbols=symbols, val_ratio=val_ratio, test_ratio=test_ratio, num_candles=num_candles,
+        day_offset=day_offset, days=days, feature_set=feature_set)
 
     dtrain_max = xgb.DMatrix(X_train, label=y_max_train, feature_names=feature_names)
     dval_max = xgb.DMatrix(X_val, label=y_max_val, feature_names=feature_names)
@@ -128,15 +110,30 @@ def get_max_dmatrix(symbols, feature_set, val_ratio, test_ratio, num_candles, da
     return dtrain_max, dval_max, dtest_max
 
 
-def train_close(params, num_rounds, early_stopping_rounds,
-                symbols, feature_set, val_ratio, test_ratio, num_candles, day_offset, days):
+def train_close(params, num_rounds, early_stopping_rounds, symbols, feature_set, val_ratio,
+                test_ratio, num_candles, day_offset, days):
     dtrain_close, dval_close, dtest_close = get_close_dmatrix(symbols=symbols,
                                                               feature_set=feature_set,
                                                               val_ratio=val_ratio,
                                                               test_ratio=test_ratio,
                                                               num_candles=num_candles,
-                                                              day_offset=day_offset,
-                                                              days=days)
+                                                              day_offset=day_offset, days=days)
+    return _train_close_with_dmatrices(params, num_rounds, early_stopping_rounds,
+                                       (dtrain_close, dval_close, dtest_close,))
+
+
+def train_close_from_saved(params, num_rounds, early_stopping_rounds, symbols, feature_set,
+                           val_ratio, test_ratio, num_candles, day_offset, days):
+    valr = int(val_ratio * 10)
+    testr = int(test_ratio * 10)
+    dmatrices = load_close_dmatrix(num_candles=num_candles, candle_period=M5,
+                                   feature_set=feature_set, num_days=days, days_offset=day_offset,
+                                   symbols=symbols, valr=valr, testr=testr)
+    _train_close_with_dmatrices(params, num_rounds, early_stopping_rounds, dmatrices)
+
+
+def _train_close_with_dmatrices(params, num_rounds, early_stopping_rounds, dmatrices):
+    dtrain_close, dval_close, dtest_close = dmatrices
     watchlist_close = [(dtrain_close, 'train_close'), (dtest_close, 'test_close'),
                        (dval_close, 'val_close')]
 
@@ -159,20 +156,32 @@ def train_close(params, num_rounds, early_stopping_rounds,
     return pred_close
 
 
-def train_max(params, num_rounds, early_stopping_rounds,
-              symbols, feature_set, val_ratio, test_ratio, num_candles, day_offset, days):
-    dtrain_max, dval_max, dtest_max = get_max_dmatrix(symbols=symbols,
-                                                      feature_set=feature_set,
-                                                      val_ratio=val_ratio,
-                                                      test_ratio=test_ratio,
+def train_max(params, num_rounds, early_stopping_rounds, symbols, feature_set, val_ratio,
+              test_ratio, num_candles, day_offset, days):
+    dtrain_max, dval_max, dtest_max = get_max_dmatrix(symbols=symbols, feature_set=feature_set,
+                                                      val_ratio=val_ratio, test_ratio=test_ratio,
                                                       num_candles=num_candles,
-                                                      day_offset=day_offset,
-                                                      days=days)
-    watchlist_max = [(dtrain_max, 'train_max'), (dtest_max, 'test_max'),
-                       (dval_max, 'val_max')]
+                                                      day_offset=day_offset, days=days)
+    return _train_max_with_dmatrices(params, num_rounds, early_stopping_rounds,
+                                     (dtrain_max, dval_max, dtest_max,))
+
+
+def train_max_from_saved(params, num_rounds, early_stopping_rounds, symbols, feature_set, val_ratio,
+                         test_ratio, num_candles, day_offset, days):
+    valr = int(val_ratio * 10)
+    testr = int(test_ratio * 10)
+    dmatrices = load_max_dmatrix(num_candles=num_candles, candle_period=M5, feature_set=feature_set,
+                                 num_days=days, days_offset=day_offset, symbols=symbols, valr=valr,
+                                 testr=testr)
+    _train_max_with_dmatrices(params, num_rounds, early_stopping_rounds, dmatrices)
+
+
+def _train_max_with_dmatrices(params, num_rounds, early_stopping_rounds, dmatrices):
+    dtrain_max, dval_max, dtest_max = dmatrices
+    watchlist_max = [(dtrain_max, 'train_max'), (dtest_max, 'test_max'), (dval_max, 'val_max')]
 
     bst_max = xgb.train(params=params, dtrain=dtrain_max, num_boost_round=num_rounds,
-                          evals=watchlist_max, early_stopping_rounds=early_stopping_rounds)
+                        evals=watchlist_max, early_stopping_rounds=early_stopping_rounds)
 
     max_best_ntree_limit = bst_max.best_ntree_limit
     feature_importances_max = bst_max.get_fscore()
