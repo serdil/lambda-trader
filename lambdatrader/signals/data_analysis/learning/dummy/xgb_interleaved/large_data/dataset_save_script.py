dcd2ccae93305c8136a0527eed9248d3a2dd4475
Sinan Erdil <hsinanerdil@gmail.com>
2018-03-04 05:35:07 +0300

    Updates to dataset_save_script.

--- lambdatrader/signals/data_analysis/learning/dummy/xgb_interleaved/large_data/dataset_save_script.py
+++ lambdatrader/signals/data_analysis/learning/dummy/xgb_interleaved/large_data/dataset_save_script.py
@@ -6,14 +6,14 @@ sdd = SplitDatasetDescriptors
 save_libsvm = True
 error_on_missing = False
 
-# d1 = sdd.sdd_1_close_mini()
-# d2 = sdd.sdd_1_max_mini()
+d1 = sdd.sdd_1_close_mini()
+d2 = sdd.sdd_1_max_mini()
 
 # d1 = sdd.sdd_1_close()
 # d2 = sdd.sdd_1_max()
 
-d1 = sdd.sdd_1_more_data_close()
-d2 = sdd.sdd_1_more_data_max()
+# d1 = sdd.sdd_1_more_data_close()
+# d2 = sdd.sdd_1_more_data_max()
 
 print('d1 train:{} val:{} test:{}'.format(d1.training.hash, d1.validation.hash, d1.test.hash))
 print('d2 train:{} val:{} test:{}'.format(d2.training.hash, d2.validation.hash, d2.test.hash))
